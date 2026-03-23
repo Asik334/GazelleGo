07f2617ae7457513bd48e187a4edff7daffd8794
Asik <asikesenalin0@gmail.com>
2026-03-23 23:02:16 +0500

    fix: remove is_admin field

--- proxy.ts
+++ proxy.ts
@@ -2,15 +2,16 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export async function proxy(req: NextRequest) {
-    if (req.nextUrl.pathname.startsWith('/dashboard')) {
-        const cookie = req.cookies.get('sb-access-token')
-        if (!cookie) {
-            return NextResponse.redirect(new URL('/login', req.url))
-        }
-    }
-    return NextResponse.next()
+  const cookie = req.cookies.get('sb-access-token')
+  if (!cookie && req.nextUrl.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/login', req.url))
+  }
+  if (!cookie && req.nextUrl.pathname.startsWith('/profile')) {
+    return NextResponse.redirect(new URL('/login', req.url))
+  }
+  return NextResponse.next()
 }
 
 export const config = {
-    matcher: ['/dashboard/:path*'],
+  matcher: ['/dashboard/:path*', '/profile/:path*'],
 }
