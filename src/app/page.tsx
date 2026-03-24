9c45e736c113e957d330acffb089cad0454ca80a
Asik <asikesenalin0@gmail.com>
2026-03-24 14:20:37 +0500

    fix: resolve merge conflict in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import { redirect } from 'next/navigation'
 
 export default function Home() {
   redirect('/login')
 }
-=======
-export { default } from 'next/dist/client/components/not-found-error'
->>>>>>> c83697ef8df092067dcb9272c434a48a484282f1
